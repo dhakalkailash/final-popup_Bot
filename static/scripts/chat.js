c7dbbec39303e6f082f8d5d7119b089570734a1a
Kailash Dhakal <dhakalkailash1997@gmail.com>
2021-08-08 11:54:22 -0500

    Temeplete ko haina hai!!

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -55,39 +55,39 @@ function firstBotMessage() {
 firstBotMessage();
 
 // Retrieves the response
-function getHardResponse(userText) {
-    let botResponse = getBotResponse(userText);
-    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
-    $("#chatbox").append(botHtml);
+// function getHardResponse(userText) {
+//     let botResponse = getBotResponse(userText);
+//     let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+//     $("#chatbox").append(botHtml);
 
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
-}
+//     document.getElementById("chat-bar-bottom").scrollIntoView(true);
+// }
 
 //Gets the text text from the input box and processes it
-function getResponse() {
-    let userText = $("#textInput").val();
+// function getResponse() {
+//     let userText = $("#text").val();
 
-    if (userText == "") {
-        userText = "I love Code Palace!";
-    }
+//     if (userText == "") {
+//         userText = "I love Code Palace!";
+//     }
 
-    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+//     let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
 
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+//     $("#text").val("");
+//     $("#chatbox").append(userHtml);
+//     document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-    setTimeout(() => {
-        getHardResponse(userText);
-    }, 1000)
+//     setTimeout(() => {
+//         getHardResponse(userText);
+//     }, 1000)
 
-}
+// }
 
 // Handles sending text via button clicks
 function buttonSendText(sampleText) {
     let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
 
-    $("#textInput").val("");
+    $("#text").val("");
     $("#chatbox").append(userHtml);
     document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
@@ -104,7 +104,7 @@ function sendButton() {
 
 
 // Press enter to send a message
-$("#textInput").keypress(function (e) {
+$("#text").keypress(function (e) {
     if (e.which == 13) {
         getResponse();
     }
